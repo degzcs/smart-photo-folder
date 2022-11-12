d3bd9e7308fd57225b8e5adc16e3e7a44113dd3e
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2022-11-11 20:27:49 -0500

    crate specs for parser (maybe will move it to its own class later)

--- model/photo_record.rb
+++ model/photo_record.rb
@@ -5,8 +5,14 @@ module Types
   include Dry.Types()
 end
 
+# TODO:
+# create a model per record that helps to sort them in proper way
+# # validate the extensions in the model
+# # validate the name of the city starts with a Capital letter
+# # validate the uniqueness of the time per photo record
+# # complete the sequence for the photo based on the list length
 class PhotoRecord < Dry::Struct
-    attribute :photo_name, Types::Strict::String
-    attribute :city_name, Types::Strict::String
-    attribute :timestamp, Types::Strict::DateTime # TODO: set time based on location: time zone
+  attribute :photo_name, Types::Strict::String
+  attribute :city_name, Types::Strict::String
+  attribute :timestamp, Types::Strict::DateTime # TODO: set time based on location: time zone
 end
--- photo_challenge.rb
+++ photo_challenge.rb
@@ -9,7 +9,6 @@ require './model/photo_record.rb'
 #
 # new photo name format:
 # '<\<photoname>>.<\<extension>>, <<city_name>>, yyyy-mm-dd, hh:mm:ss",
-# :w
 # where '<<photo_name>>', '<\<extension>>' and, '<<city_name>>' consist only of letters of the English alphabet.
 #
 #
@@ -17,13 +16,12 @@ require './model/photo_record.rb'
 
 def solution(string)
   # convert the string input into an array of M elements
-  parse(string)
+  photo_records = parse(string)
   # validate that the array is not longer than 100 elements and less than 1
-  # create a model per record that helps to sort them in proper way
-  # # validate the extensions in the model
-  # # validate the name of the city starts with a Capital letter
-  # # validate the uniqueness of the time per photo record
-  # # complete the sequence for the photo based on the list length
+  raise 'The input is too larger' if photo_records.size > 100
+
+  # TODO:
+  # group photos by city
   # create a system for tag each photo in a sequential way
 end
 
@@ -36,29 +34,8 @@ def parse(string)
                                      city_name: city_name,
                                      timestamp: DateTime.parse(timestamp))
   end
-  binding.pry
   photo_records
 end
-
-raw_inputs = <<-PHT
-photo.jpg, Krakow, 2013-09-05 14:08:15
-Mike.png, London, 2015-06-20 15:13:22
-myFriends.png, Krakow, 2013-09-05 14:07:13
-Eiffel.jpg, Florianopolis, 2015-07-23 08:03:02
-pisatower.jpg, Florianopolis, 2015-07-22 23:59:59
-BOB.jpg, London, 2015-08-05 00:02:03
-notredame.png, Florianopolis, 2015-09-01 12:00:00
-me.jpg, Krakow, 2013-09-06 15:40:22
-a.png, Krakow, 2016-02-13 13:33:50
-b.jpg, Krakow, 2016-01-02 15:12:22
-c.jpg, Krakow, 2016-01-02 14:34:30
-d.jpg, Krakow, 2016-01-02 15:15:01
-e.png, Krakow, 2016-01-02 09:49:09
-f.png, Krakow, 2016-01-02 10:55:32
-g.jpg, Krakow, 2016-02-29 22:13:11
-PHT
-
-solution(raw_inputs)
 
 outputs = [
 'Krakow02.jpg',
